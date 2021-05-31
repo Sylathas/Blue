8770b1cc1951482d3dc9fe05536d7790fd14b306
Sylathas <abate.niccolo@gmail.com>
2021-05-31 15:33:25 +0200

    Updates
    Added all graphical elements of the visual progression part.

--- dialogues.js
+++ dialogues.js
@@ -1,21 +1,55 @@
 //Arrays with dialogues
-export var selfDialogue = [
-  { m: "I have made my first good decision" },
-  { m: "I have made my second good decision" },
-  { m: "I have made my third good decision" },
-  { m: "This is my final self dialogue" }
-];
+export let selfDialogue = {
+  Eo: [
+    ["There seems to be a path...", "Let's try following it","This thing looks unstable, yet charming; it's changing shape slightly"],
+    ["There is something about this path, it doesn't seem to lead anywhere","The thing is still changing shape, a little faster, it seems to react to the path"],
+    ["This place looks familiar...","Either you're going crazy, or the the path is looping on itself","The shifting is uncontrollable now, something is about to happen"],
+    ["Looking from here the path is forming a shape, it looks like a perfect circle","the thing it still now, it looks like a perfect sphere","For some reason, it looks peacful now, how soothing"]
+  ],
+  Maisie: [
+  ],
+  Pa: [
+  ],
+  Tedd: [
+  ],
+  Joe: [
 
-export var characterDialogue = [
-  { m: "You have made your first good decision" },
-  { m: "You have made your second good decision" },
-  { m: "You have made your third good decision" },
-  { m: "This is the final character dialogue" }
-];
+  ],
+  Neutrum: [
+
+  ],
+  Swarm: [
+
+  ]
+};
+
+export var characterDialogue = {
+  Eo: [
+    [" O XO J O KSA SK K K AS A A KD JD SJEU J JSK"],
+    ["OJEIJCIMDOP4JIAPSCMAKOSMCIRSNKDLCMORMCOSMRICSI"],
+    ["OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"],
+    ["I TOPI NON AVEVANO NIPOTI"]
+  ],
+  Maisie: [
+  ],
+  Pa: [
+  ],
+  Tedd: [
+  ],
+  Joe: [
+
+  ],
+  Neutrum: [
+
+  ],
+  Swarm: [
+
+  ]
+};
 
 export var wrongSelfDialogue = [
-  { m: "Wrong self dialogue 1" },
-  { m: "Wrong self dialogue 2" }
+  { m: "The thing looks angry, this doesn't feel right" },
+  { m: "It looks angry, when it's angry the shape look to lose stability" }
 ];
 
 export var wrongCharacterDialogue = [
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-import {selfDialogue, characterDialogue, wrongSelfDialogue, wrongCharacterDialogue} from "./dialogues.js";
+import {selfDialogue, characterDialogue, wrongSelfDialogue, wrongCharacterDialogue} from "./dialogues.js"
+
+var start = true;
+
+$( document ).ready(function() {
+  introduction();
+});
 
 //FUNCTIONS ABOUT THE PROGRESSION OF THE STORY
 function introduction(){
+  //Start button
+  let startButton = $("<div></div>");
+  startButton.attr({ "id": "introductionButton", "class": "button", 'onclick': `$( ".button" ).remove(); sectionProgress('Eo', 'visual', 0);`});
+  $("body").append(startButton);
 }
 
-function characterChoice(part){
+window.characterChoice = function(part){
   //Create the divs containing the sprites and QR's of the characters
   if(part == 1){
     createCharacter("Pa");
@@ -19,7 +29,7 @@ function characterChoice(part){
   }
 }
 
-function sectionProgress(character, communication, progress) {
+window.sectionProgress = function(character, communication, progress) {
   if(progress < 4){
     //Dialogue and selection of monuments
     console.log(`Section progress reached, with progress ${progress}`);
@@ -39,7 +49,7 @@ function sectionProgress(character, communication, progress) {
   }
 }
 
-function ending(){
+window.ending = function(){
   //Remove old dialogues and buttons
   $( ".dialogue, .button" ).remove();
   //Self Dialogue
@@ -50,74 +60,161 @@ function ending(){
 
 //FUNCTIONS ABOUT DIALOGUES, CHOICES AND DIV CREATION
 
-function createCharacter(character){
+window.createCharacter = function(character){
   //Create parent div
   //Create Div with as background image the sprite of the character
   //Create Div with as background the QR of the character
 }
 
-function progressDialogue(character, communication, progress){
+window.progressDialogue = function(character, communication, progress){
+  startvideo();
   //Remove old dialogues and buttons
-  $( ".dialogue, .button" ).remove();
+  $( ".button" ).remove();
   progress++;
   //Prototyping help
-  prototyping(progress);
+  //prototyping(progress);
   //Self Dialogue
-  let selfText = $("<p></p>").text(selfDialogue[progress-1].m);
-  selfText.attr({"id": "selfDialogue", "class": "dialogue" });
+  for(var i = 0; i < selfDialogue[character][progress-1].length; i++){
+    let selfText = $("<p></p>").html("<span class='bold'>Me: </span>" + selfDialogue[character][progress-1][i]);
+    selfText.attr({"id": "selfDialogue", "class": "dialogue" });
+    $("#dialogueBox").append(selfText);
+  }
   //Character Dialogue
-  let characterText = $("<p></p>").text(characterDialogue[progress-1].m);
-  characterText.attr({"id": "characterDialogue", "class": "dialogue" });
+  for(var i = 0; i < characterDialogue[character][progress-1].length; i++){
+    let characterText = $("<p></p>").html("<span class='bold'>" + character + ": </span>" + characterDialogue[character][progress-1][i]);
+    characterText.attr({"id": "characterDialogue", "class": "dialogue" });
+    characterText.addClass(character);
+    $("#dialogueBox").append(characterText);
+  }
   //Continue button
   let continueButton = $("<button></button>").text("Continue");
   continueButton.attr({ "id": "continueButton", "class": "button", 'onclick': `sectionProgress("${character}", "${communication}", ${progress})` });
   //Add them to the body
-  $("body").append(selfText, characterText, continueButton);
+  $("body").append(continueButton);
 }
 
-function wrongDialogue(character, communication, progress){
+window.wrongDialogue = function(character, communication, progress){
+  startvideo();
   //Remove old dialogues and buttons
-  $( ".dialogue, .button" ).remove();
+  $( ".button" ).remove();
   progress = 0;
   //Prototyping help
-  prototyping(progress);
+  //prototyping(progress);
   //Self Dialogue
-  let selfText = $("<p></p>").text(wrongSelfDialogue[Math.floor(Math.random())].m);
+  let selfText = $("<p></p>").html("<span class='bold'>Me: </span>" + wrongSelfDialogue[Math.floor(Math.random())].m);
   selfText.attr({"id": "selfDialogue", "class": "dialogue" });
   //Character Dialogue
-  let characterText = $("<p></p>").text(wrongCharacterDialogue[Math.floor(Math.random())].m);
-  characterText.attr({"id": "characterDialogue", "class": "dialogue" });
+  let characterText = $("<p></p>").html("<span class='bold'>" + character + ": </span>" + wrongCharacterDialogue[Math.floor(Math.random())].m);
+  characterText.attr({"id": "characterDialogue", "class": `dialogue` });
+  characterText.addClass(character);
   //Continue button
   let continueButton = $("<button></button>").text("Continue");
   continueButton.attr({ "id": "continueButton", "class": "button", 'onclick': `sectionProgress("${character}", "${communication}", ${progress})` });
   //Add them to the body
-  $("body").append(selfText, characterText, continueButton);
+  $("body").append(continueButton);
+  $("#dialogueBox").append(selfText, characterText);
 }
 
-function selection(character, communication, progress){
+window.selection = function(character, communication, progress){
+  stopvideo();
+  let buttons = ["choiceOne", "choiceTwo", "choiceThree"];
   //Remove old dialogues and buttons
-  $( ".dialogue, .button" ).remove();
+  $( ".button" ).remove();
   //Prototyping help
-  prototyping(progress);
+  //prototyping(progress);
+
+  if(start){
+    //Dialogue div
+    let dialogueBox = $("<div></div>");
+    dialogueBox.attr({"id": "dialogueBox"});
+    //Character
+    let characterDiv = $("<div></div>");
+    characterDiv.attr({"id": "character"});
+    let characterAnimation = $(`<img id="characterAnimation" src='./Assets/Eo.gif' style='width:auto; height:100%; position: relative; left: 10%; transition: .5s;'></video>`);
+    let characterQR = $("<img id='QR' src='./Assets/QR/Eo.png'></img>");
+
+    $("body").append(dialogueBox, characterDiv);
+    $("#character").append(characterAnimation, characterQR);
+    start = false;
+  }
   //Self Dialogue
-  let selfText = $("<p></p>").text("Which monument should I choose?");
+  let selfText = $("<p></p>").html("<span class='bold'>Me: </span> Which of these monuments do I want to go to?");
   selfText.attr({"id": "selfDialogue", "class": "dialogue" });
   //Character Dialogue
   let characterText = $("<p></p>").text("");
-  characterText.attr({"id": "characterDialogue", "class": "dialogue" });
+  characterText.attr({"id": "characterDialogue", "class": `dialogue` });
+  characterText.addClass(character);
+
+  buttons = shuffleArray(buttons);
+
   //Create the three buttons with the choices
   let choiceOne = $("<button></button>").text("Wrong");
-  choiceOne.attr({"id": "choiceOne", "class": "button", 'onclick': `wrongDialogue("${character}", "${communication}", ${progress})`});
+  choiceOne.attr({"id": buttons[0], "class": communication + " button", 'onclick': `wrongDialogue("${character}", "${communication}", ${progress})`});
+  choiceOne.css("background-image", "url(./Assets/totem_a.png)");
   let choiceTwo = $("<button></button>").text("True");
-  choiceTwo.attr({"id": "choiceTwo", "class": "button", 'onclick': `progressDialogue("${character}", "${communication}", ${progress})`});
+  choiceTwo.attr({"id": buttons[1], "class": communication + " button", 'onclick': `progressDialogue("${character}", "${communication}", ${progress})`});
+  choiceTwo.css("background-image", "url(./Assets/totem_b.png)");
   let choiceThree = $("<button></button>").text("Wrong");
-  choiceThree.attr({"id": "choiceThree", "class": "button", 'onclick': `wrongDialogue("${character}", "${communication}", ${progress})`});
-  //Add them to the body
-  $("body").append(selfText, characterText, choiceOne, choiceTwo, choiceThree);
+  choiceThree.attr({"id": buttons[2], "class": communication + " button", 'onclick': `wrongDialogue("${character}", "${communication}", ${progress})`});
+  choiceThree.css("background-image", "url(./Assets/totem_c.png)");
+  //Add them to the page
+  $("body").append(choiceOne, choiceTwo, choiceThree);
+  $("#dialogueBox").append(selfText, characterText);
+  $('#dialogueBox').scrollTop($('#dialogueBox')[0].scrollHeight);
+
+  $("#characterAnimation, #QR").hover(function(){
+    $("#characterAnimation").css({"opacity": `.8`});
+    $("#QR").css({"opacity": `1`});
+  });
+  $( "#characterAnimation, #QR" ).mouseout(function() {
+    $("#characterAnimation").css({"opacity": `1`});
+    $("#QR").css({"opacity": `0`});
+  });
 }
 
-function prototyping(progress){
+//STRUMENTAL FUNCTIONS
+
+window.shuffleArray = function(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+window.startvideo = function(){
+  $('#video')[0].play();
+  var i = 1/16;
+  document.querySelector('video').playbackRate = i;
+  var intervalId = window.setInterval(function(){
+    i = i * 1.5;
+    document.querySelector('video').playbackRate = i;
+    if(i > 1){
+      clearInterval(intervalId);
+      document.querySelector('video').playbackRate = 1;
+    }
+  }, 150);
+}
+
+window.stopvideo = function(){
+  var i = 1;
+  var j = 1;
+  var intervalId = window.setInterval(function(){
+    i = i / 1.5;
+    j++;
+    document.querySelector('video').playbackRate = i;
+    if(j == 5){
+      clearInterval(intervalId);
+      $('#video')[0].pause();
+    }
+  }, 150);
+}
+
+window.prototyping = function(progress){
   //Add progress to top left
+  $("#progressDialogue").remove();
   let progressText = $("<p></p>").text(`Progress: ${progress}`);
   progressText.attr({"id": "progressDialogue", "class": "dialogue" });
   $("body").append(progressText);
